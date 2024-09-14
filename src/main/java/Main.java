5c15012ff9550cc0cf0893926d80a4b7a5353888
ososabe <ososabe@contraloria.cl>
2024-09-14 20:57:02 -0300

    REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
+
+    private static final String COMMAND_NOT_FOUND = ": command not found";    
+    private static final String PROMPT = "$ ";  
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
          System.out.print("$ ");
-
+        
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        do {
+            System.out.println(input + COMMAND_NOT_FOUND);
+            System.out.print(PROMPT);
+            input = scanner.nextLine();
+        } while (!input.matches("")); 
+
+        scanner.close();
     }
 }
