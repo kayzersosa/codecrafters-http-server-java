d8403516e95c8310adc029a8c24cd167e81c9e98
ososabe <ososabe@contraloria.cl>
2024-09-14 22:45:45 -0300

    The echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     private static final String COMMAND_NOT_FOUND = ": command not found";    
     private static final String PROMPT = "$ ";  
     private static final String EXIT = "exit 0";
+    private static final String ECHO = "echo";
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
          System.out.print("$ ");
@@ -12,14 +13,41 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         do {
+
             if (input.equals(EXIT)) {
                 break;
             }
-            System.out.println(input + COMMAND_NOT_FOUND);
+         
+            if (isEcho(input)) {
+                System.out.println(getEchoMessage(input));
+            } else {
+                System.out.println(input + COMMAND_NOT_FOUND);
+            }
             System.out.print(PROMPT);
+         
+         
             input = scanner.nextLine();
         } while (!input.matches("")); 
 
         scanner.close();
+    }
+
+
+    private static Boolean isEcho(String input) {
+        if (input.length() < 4){
+            return false;
+        }
+        if (ECHO.equals(input.substring(0, 4)) && input.charAt(4) == ' ') {
+            return true;
+        }
+        return false;
+    }
+
+    private static String getEchoMessage(String input) {
+        if (input.length() > 4 ) {
+            return input;
+        }
+        return ECHO;
+        
     }
 }
