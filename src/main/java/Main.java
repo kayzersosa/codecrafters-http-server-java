fbc1f2b8be284d3996d191664b2c51bedfb91ecd
ososabe <ososabe@contraloria.cl>
2024-10-04 18:52:53 -0300

    The type builtin: executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,17 +38,17 @@ public class Main {
                 case "echo":
                   System.out.println(getEchoMessage(input));
                 case "type":
-                    if (command.equals(builtins.get(0)) ||
-                        command.equals(builtins.get(1)) ||
-                        command.equals(builtins.get(2))) {
-                         System.out.println(command + " is a shell builtin");
+                    if (parameter.equals(builtins.get(0)) ||
+                        parameter.equals(builtins.get(1)) ||
+                         parameter.equals(builtins.get(2))) {
+                         System.out.println(parameter + " is a shell builtin");
                      } else {
-                        System.out.println(command + ": not found");
-                        path = getPath(command);
+                        //System.out.println(command + ": not found");
+                        path = getPath(parameter);
                         if (path != null) {
-                            System.out.println(command + " is " + path);
+                            System.out.println(parameter + " is " + path);
                         } else {
-                                System.out.println(command + ": not found");
+                                System.out.println(parameter + ": not found");
                         }
                      }
                 default:
@@ -56,58 +56,17 @@ public class Main {
             }
 
         } 
-
+        
     }
 
-    private static Boolean isEcho(String input) {
-        if (input.length() <= 4) {
-            return false;
-        }
-        if (ECHO.equals(input.substring(0, 4)) && input.charAt(4) == ' ') {
-            return true;
-        }
-        return false;
-    }
-
-    private static String getEchoMessage(String input) {
+       private static String getEchoMessage(String input) {
         if (input.length() > 4) {
             return input.substring(5);
         }
         return ECHO;
     }
 
-    private static Boolean isType(String input) {
-
-        try {
-            if (input.substring(0, 4).equals("type") && input.charAt(4) == ' ') {
-                return true;
-            }
-
-            if (input.equals(TYPECOMMAND[0]) || input.equals(TYPECOMMAND[1]) || input.equals(TYPECOMMAND[2])
-                    || input.equals(TYPECOMMAND[3])) {
-                return true;
-            }
-
-        } catch (Exception e) {
-            return false;
-        }
-        return false;
-    }
-
-    private static String getTypeMessage(String input) {
-
-        if (input.equals(TYPECOMMAND[0])) {
-            return "echo is a shell builtin";
-        } else if (input.equals(TYPECOMMAND[1])) {
-            return "exit is a shell builtin";
-        } else if (input.equals(TYPECOMMAND[2])) {
-            return "cat is /bin/cat";
-        } else if (input.equals(TYPECOMMAND[3])) {
-            return "type is a shell builtin";
-        }
-        return input.substring(5) + ": not found";
-    }
-
+    
     private static String getPath(String parameter) {
 
         for (String path : System.getenv("PATH").split(":")) {
@@ -119,15 +78,7 @@ public class Main {
         return null;
     }
 
-    private static void getTypePath(String input, String path) {
-        if (path != null) {
-            System.out.println(input.substring(5) + " is " + path);
-        } else {
-            System.out.println(input.substring(5) + ": not found");
-        }
-    }
-
-    private static List<String> commands() {
+       private static List<String> commands() {
         List<String> commands = new ArrayList<>();
         commands.add("exit");
         commands.add("echo");
